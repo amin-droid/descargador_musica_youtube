32a48126eebc91f17c626be06116fc35e72754d9
amin-droid <dahmoni22@gmail.com>
2022-08-29 18:25:40 +0200

    ADD que se vea el resultado del link falta tocar mucho css aun

--- descargar_musica/downloader/views.py
+++ descargar_musica/downloader/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from moviepy.editor import VideoFileClip
 from .forms import VideoLink
 import os
+import urllib.request
 # Create your views here.
 from pytube import YouTube
 # from selenium import webdriver
@@ -13,13 +14,26 @@ def index(request):
     
 
 def searchbar(request):
+    resolutions = []
+    SAVE_PATH = "f:/" #to_do 
     if request.method == 'POST':
         search = request.POST['search']
         yt = YouTube(search)
-        t=yt.streams.filter(progressive=True, file_extension='mp4').first()
+        # t=yt.streams.get_highest_resolution()
+        t = yt.thumbnail_url
         #t[0].download()
-        t.download()
-        video = VideoFileClip(os.path.join(t.default_filename,"to","movie.mp4"))
-        video.audio.write_audiofile(os.path.join(t.title,"to","movie_sound.mp3"))
-        video.audio.write_audiofile
-    return render(request,'index.html')
+        # t.download(SAVE_PATH)
+        # video = VideoFileClip(os.path.join(t.default_filename,"to","movie.mp4"))
+        # # video.audio.write_audiofile(os.path.join(t.title,"to","movie_sound.mp3"))
+        # video.audio.write_audiofile
+        print(t)
+        for stream in yt.streams.all():
+            print(type(stream.resolution))
+            if stream.resolution != None:
+
+                resolutions.append(stream.resolution)
+        context ={
+            'search':yt,
+            'resolutions':resolutions
+       }
+    return render(request,'index.html',context)
